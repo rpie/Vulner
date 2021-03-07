24c04fcdb5b020c2a19e054affe9ab99d7ee4379
HellSec <57877984+rpie@users.noreply.github.com>
2021-03-07 15:12:22 -0800

    updated things

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import requests, json
-from modules import test
+import requests, json, os
+from modules import cloudssp
 from colorama import Fore
-
-def print_error(a):
-    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTRED_EX}ERRO {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
-
-def print_sucess(a):
-    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTGREEN_EX}INFO {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
-
-def print_info(a):
-    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTYELLOW_EX}INFO {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
-
-def print_warning(a):
-    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTYELLOW_EX}WARN {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
 
 def logo():
     return f''' {Fore.WHITE}{Fore.LIGHTBLACK_EX}          {Fore.LIGHTWHITE_EX}0{Fore.LIGHTBLACK_EX}
@@ -22,7 +10,20 @@ def logo():
 {Fore.MAGENTA}   ===== {Fore.LIGHTBLACK_EX}   \_/ \_,_|_|_||_\___|_|   {Fore.LIGHTGREEN_EX}0.1{Fore.WHITE}
 '''
 
+def clear():
+    os.system('cls')
+
 def main():
+    clear()
     print(logo())
-    print(print_sucess('Starting Vulner...'))
-    test('a')
+    while True:
+        cmd = input(f'{Fore.WHITE}[{Fore.MAGENTA} INPU{Fore.WHITE} ] {Fore.GREEN}~>{Fore.WHITE} ')
+
+        if 'cloudssp' in cmd.lower():
+            target = cmd.split(' ')[1]
+            cloudssp(target)
+        if cmd.lower() == 'clear' or cmd.lower() == 'cls':
+            main()
+        if cmd.lower() == 'exit':
+            exit(-1)
+main()
--- modules.py
+++ modules.py
@@ -1,4 +1,43 @@
 import requests
+from colorama import Fore
+from bs4 import BeautifulSoup
 
-def test(a):
-    print(f'test {a}')
+def print_error(a):
+    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTRED_EX}ERRO {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
+
+def print_sucess(a):
+    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTGREEN_EX}INFO {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
+
+def print_info(a):
+    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTYELLOW_EX}INFO {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
+
+def print_warning(a):
+    print(f'{Fore.LIGHTWHITE_EX}[ {Fore.LIGHTYELLOW_EX}WARN {Fore.LIGHTWHITE_EX}] {Fore.RESET}{a}')
+
+def cloudssp(target):
+    if 'http' not in target:
+        print_error('Please enter a valid website ( https://test.com )')
+        return
+    print_info('Starting module CloudSSP..')
+    print_info(f'Set target : {target}')
+    print_info(f'Testing target for vulnerable page')
+
+    try:
+        req = requests.get(f'{target}//mailman/listinfo/mailman')
+    except:
+        print_warning('Target seems to be offline')
+        return
+    if req.status_code == 200:
+        try:
+            soup = BeautifulSoup(req.text, 'html.parser')
+            backend = soup.find_all('a')
+            backend = requests.get(f'{backend[0].get("href")}', stream=True)
+            backend = backend.raw._connection.sock.getpeername()[0]
+            print_info(f'Target {Fore.LIGHTGREEN_EX}IS{Fore.WHITE} vulnerable')
+            print_sucess(f'Backend IP : {Fore.MAGENTA}{backend}{Fore.WHITE}')
+        except:
+            print_error('Target isn\'t vulnerable')
+
+def rcetest(target):
+    print_info('Starting module CloudSSP..')
+    print_info(f'Set Target : {target}')
